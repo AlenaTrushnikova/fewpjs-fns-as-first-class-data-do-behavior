bcda374e057b9970a3dedb48f08ada67b4719361
Alena Trushnikova <alena.yu.t@gmail.com>
2021-01-12 12:17:27 -0800

    done

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+// 1) Returns Good Morning before 12pm
+// 2) Returns Good Afternoon between 12pm and 5pm
+// 3) Returns Good Evening after 5pm
+function greet(timeHhMm) {
+  let i = parseInt(timeHhMm, 10)
+  if (i < 12) return 'Good Morning'
+  else if (i > 17) return 'Good Evening'
+  else return 'Good Afternoon'
+}
+
 /* Write your implementation of displayMessage() */
+// updates the DOM with an appropriate message
+
+function displayMessage(text){
+  document.getElementById('greeting').innerText = text
+}
